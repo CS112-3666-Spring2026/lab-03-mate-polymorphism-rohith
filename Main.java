e0200aaa42b3e986015063d2879cc09fa56096c4
rmekala3623 <rmekala3623@student.miracosta.edu>
2026-05-17 21:16:31 +0000

    Finished assignment, edited main.java file and finished up all tasks

--- Main.java
+++ Main.java
@@ -19,6 +19,7 @@ public class Main
 			System.out.println("3) Exit");
 			System.out.print(">>");
 			choice = keyboard.nextInt();
+			keyboard.nextLine();
 
 			switch (choice) {
 				case 1: // Tea
@@ -37,9 +38,71 @@ public class Main
 					System.out.println("Your tea order has been added: " + inventory[count]);
 					count++;
 					break;
+				case 2: // YerbaMate
+					System.out.print("Enter name      : ");
+					name = keyboard.nextLine();
+					System.out.print("Enter ounces    : ");
+					ounces = keyboard.nextInt();
+					System.out.print("Enter price     $ ");
+					price = keyboard.nextDouble();
+					System.out.print("Enter brew temperature (in Celsius): ");
+					brewTemp = keyboard.nextInt();
+
+					// Create a YerbaMate, put into array
+					inventory[count] = new YerbaMate(name, ounces, price, brewTemp, 0);
+
+					System.out.println("Your YerbaMate order has been added: " + inventory[count]);
+					count++;
+					break;
+				case 3: // Exit Case
+					System.out.println("\nAll Drinks: ");
+					Main.printDrinks(inventory, count);
+
+					System.out.printf("\nAverage Price = $%.2f\n", Main.findAveragePrice(inventory, count));
+
+					YerbaMate mostExpensive = Main.findHighestPricedYerbaMate(inventory, count);
+					if (mostExpensive != null) {
+						System.out.printf("\nHighest Priced Mate = %s%n", mostExpensive);
+					} else {
+						System.out.println("No Yerba Mate found in price of drinks.");
+					}
+
+					break;
+				default:
+					System.out.println("ERROR: not a valid choice. Please try again.\n");
 
 			}
 
 		} while (choice != 3);
+		keyboard.close();
+	}
+
+	public static void printDrinks(CaffeinatedBeverage[] drinks, int numDrinks) {
+		for (int i = 0; i < numDrinks; i++) {
+			System.out.println("Drink #" + (i + 1) + " = " + drinks[i]);
+		}
+	}
+
+	public static double findAveragePrice(CaffeinatedBeverage[] drinks, int numDrinks) {
+		double totalPrice = 0;
+		for (int i = 0; i < numDrinks; i++) {
+			totalPrice += drinks[i].getPrice();
+		}
+		return totalPrice / numDrinks;
+	}
+
+	public static YerbaMate findHighestPricedYerbaMate(CaffeinatedBeverage[] drinks, int numDrinks) {
+		YerbaMate mate = new YerbaMate(), highestPrice = null;
+
+		for (int i = 0; i < numDrinks; i++) {
+			if (drinks[i].getClass() == mate.getClass()) {
+				if (highestPrice == null) {
+					highestPrice = (YerbaMate) drinks[i];
+				} else if (highestPrice.getPrice() < drinks[i].getPrice()) {
+					highestPrice = (YerbaMate) drinks[i];
+				}
+			}
+		}
+		return highestPrice;
 	}
 }
